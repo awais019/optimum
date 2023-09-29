812f8f9c3425df920184e9178ab17dc910a2be9f
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-09-29 12:47:01 +0500

    Finished onboarding

--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -1,8 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:optimum/app_colors.dart';
+import 'dart:async';
 
-class OnBoarding extends StatelessWidget {
+class OnBoarding extends StatefulWidget {
   const OnBoarding({super.key});
+
+  @override
+  State<OnBoarding> createState() => _OnBoardingState();
+}
+
+class _OnBoardingState extends State<OnBoarding> {
+  final Widget image1 =
+      const Image(image: AssetImage("assets/onboarding1.png"));
+
+  final Widget image2 =
+      const Image(image: AssetImage("assets/onboarding2.png"));
+
+  final Widget image3 =
+      const Image(image: AssetImage("assets/onboarding3.png"));
+
+  PageController pageController = PageController();
+  int currentPage = 0;
+  bool forward = true;
+  late Timer timer;
+
+  @override
+  void initState() {
+    super.initState();
+    timer = Timer.periodic(const Duration(seconds: 2), (timer) {
+      if (forward) {
+        currentPage = (currentPage + 1) % 3;
+        if (currentPage == 3 - 1) {
+          forward = false;
+        }
+      } else {
+        currentPage = (currentPage - 1 + 3) % 3;
+        if (currentPage == 0) {
+          forward = true;
+        }
+      }
+      pageController.animateToPage(
+        currentPage,
+        duration: const Duration(milliseconds: 1000),
+        curve: Curves.easeInOut,
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +74,14 @@ class OnBoarding extends StatelessWidget {
         child: Column(
           children: [
             const SizedBox(height: 24.0),
-            const Image(image: AssetImage("assets/onboarding1.png")),
+            Expanded(
+              flex: 5,
+              child: PageView(
+                controller: pageController,
+                children: [image1, image2, image3],
+              ),
+            ),
+            const SizedBox(height: 24.0),
             const SizedBox(height: 24.0),
             const Text("Your fitness revolution starts here!!",
                 style: TextStyle(
