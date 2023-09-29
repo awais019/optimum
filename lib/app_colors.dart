890898d109c48406c6eb0954f2314930ed2f641e
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-09-29 11:12:06 +0500

    Added first onboarding screen

--- lib/app_colors.dart
+++ lib/app_colors.dart
@@ -3,4 +3,7 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color primaryColor = Color(0xFFE67E22);
   static const Color ellipse = Color(0x05261506);
+  static const Color darkNeutrals500 = Color(0xFF261506);
+  static const Color darkNeutral100 = Color(0xFFBCB6B2);
+  static const Color light50 = Color(0xFFFFFEFD);
 }
--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -19,8 +19,81 @@ class OnBoarding extends StatelessWidget {
         centerTitle: true,
         elevation: 0.0,
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(24.0),
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          children: [
+            const SizedBox(height: 24.0),
+            const Image(image: AssetImage("assets/onboarding1.png")),
+            const SizedBox(height: 24.0),
+            const Text("Your fitness revolution starts here!!",
+                style: TextStyle(
+                  color: AppColors.darkNeutrals500,
+                  fontSize: 26.0,
+                  height: 1.2,
+                  fontWeight: FontWeight.bold,
+                )),
+            const SizedBox(height: 24.0),
+            const Text(
+                "Track your progress with real-time monitoring while enjoying expert-designed exercises.",
+                style: TextStyle(
+                  color: AppColors.darkNeutral100,
+                  fontSize: 16.0,
+                  height: 1.5,
+                  fontWeight: FontWeight.w500,
+                )),
+            const SizedBox(height: 32.0),
+            SizedBox(
+              width: double.infinity,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(40.0),
+                child: TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    backgroundColor: AppColors.primaryColor,
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 16.0, horizontal: 32.0),
+                  ),
+                  child: const Text("Get Started",
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        height: 1.5,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.light50,
+                      )),
+                ),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            SizedBox(
+              width: double.infinity,
+              child: TextButton(
+                onPressed: () {},
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 16.0, horizontal: 32.0),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(40.0),
+                    border: Border.all(
+                      color: AppColors.primaryColor,
+                      width: 1.0,
+                    ),
+                  ),
+                  child: const Center(
+                    child: Text("Log in",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          height: 1.5,
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.primaryColor,
+                        )),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
