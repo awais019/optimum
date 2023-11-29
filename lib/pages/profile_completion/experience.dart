885e0c076c73f4e6b33b7c30dfb2a272926ccf0e
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-11-29 21:12:35 +0500

    Removed uneccessary themedata

--- lib/pages/profile_completion/experience.dart
+++ lib/pages/profile_completion/experience.dart
@@ -113,169 +113,163 @@ class _ExperienceState extends State<Experience> {
       ),
       body: Padding(
         padding: const EdgeInsets.fromLTRB(24.0, 0, 24.0, 24.0),
-        child: Theme(
-          data: ThemeData(
-            unselectedWidgetColor: AppColors.darkNeutrals100,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 48.0),
-              RichText(
-                  text: const TextSpan(
-                      style: TextStyle(
-                        color: AppColors.darkNeutrals500,
-                        fontSize: 26.0,
-                        fontWeight: FontWeight.w700,
-                        height: 1.23,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 48.0),
+            RichText(
+                text: const TextSpan(
+                    style: TextStyle(
+                      color: AppColors.darkNeutrals500,
+                      fontSize: 26.0,
+                      fontWeight: FontWeight.w700,
+                      height: 1.23,
+                    ),
+                    children: [
+                  TextSpan(text: "Experience and "),
+                  TextSpan(
+                      text: "Verification Proof",
+                      style: TextStyle(color: AppColors.primaryColor)),
+                ])),
+            const SizedBox(height: 8.0),
+            const Text(
+                "Please provide your experience and degree or certificate to verify your specialization",
+                style: TextStyle(
+                    color: AppColors.darkNeutrals100,
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w500,
+                    height: 1.5)),
+            const SizedBox(height: 32.0),
+            Form(
+                key: _formKey,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TextFormField(
+                        controller: experienceController,
+                        decoration: InputDecoration(
+                            hintText: 'Years of experience',
+                            hintStyle: const TextStyle(
+                                color: AppColors.darkNeutrals100,
+                                fontSize: 16.0,
+                                height: 1.5,
+                                fontWeight: FontWeight.w500),
+                            focusedBorder: const OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: AppColors.primaryColor, width: 1.0),
+                            ),
+                            focusedErrorBorder: const OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: AppColors.primaryColor, width: 1.0),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: const BorderSide(
+                                  color: AppColors.darkNeutrals50, width: 1.0),
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
+                            errorBorder: const OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Colors.red, width: 1.0))),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Experience is required";
+                          } else if (int.tryParse(value) == null) {
+                            return "Experience must be a number";
+                          }
+                          return null;
+                        },
                       ),
-                      children: [
-                    TextSpan(text: "Experience and "),
-                    TextSpan(
-                        text: "Verification Proof",
-                        style: TextStyle(color: AppColors.primaryColor)),
-                  ])),
-              const SizedBox(height: 8.0),
-              const Text(
-                  "Please provide your experience and degree or certificate to verify your specialization",
-                  style: TextStyle(
-                      color: AppColors.darkNeutrals100,
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.w500,
-                      height: 1.5)),
-              const SizedBox(height: 32.0),
-              Form(
-                  key: _formKey,
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        TextFormField(
-                          controller: experienceController,
-                          decoration: InputDecoration(
-                              hintText: 'Years of experience',
-                              hintStyle: const TextStyle(
-                                  color: AppColors.darkNeutrals100,
+                      const SizedBox(height: 24.0),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            shadowColor: const Color(0x261b0614),
+                            elevation: 10,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50.0),
+                            ),
+                          ),
+                          onPressed: () async {
+                            FilePickerResult? result =
+                                await FilePicker.platform.pickFiles(
+                              type: FileType.custom,
+                              allowedExtensions: ['jpg', 'pdf', 'doc'],
+                            );
+                            if (result != null) {
+                              setState(() {
+                                _file = File(result.files.single.path!);
+                              });
+                            }
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 24.0, vertical: 16.0),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Image.asset("assets/icons/upload.png"),
+                                const SizedBox(width: 8.0),
+                                const Text("Certification",
+                                    style: TextStyle(
+                                      color: AppColors.primaryColor,
+                                      fontSize: 16.0,
+                                      height: 1.5,
+                                      fontWeight: FontWeight.w500,
+                                    )),
+                              ],
+                            ),
+                          )),
+                      const SizedBox(height: 24.0),
+                      if (_file.path.isNotEmpty)
+                        Row(
+                          children: [
+                            Image.asset("assets/icons/upload.png"),
+                            const SizedBox(width: 8.0),
+                            Text(_file.path.split('/').last,
+                                style: const TextStyle(
+                                  color: AppColors.primaryColor,
                                   fontSize: 16.0,
                                   height: 1.5,
-                                  fontWeight: FontWeight.w500),
-                              focusedBorder: const OutlineInputBorder(
-                                borderSide: BorderSide(
-                                    color: AppColors.primaryColor, width: 1.0),
-                              ),
-                              focusedErrorBorder: const OutlineInputBorder(
-                                borderSide: BorderSide(
-                                    color: AppColors.primaryColor, width: 1.0),
-                              ),
-                              enabledBorder: OutlineInputBorder(
-                                borderSide: const BorderSide(
-                                    color: AppColors.darkNeutrals50,
-                                    width: 1.0),
-                                borderRadius: BorderRadius.circular(8.0),
-                              ),
-                              errorBorder: const OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                      color: Colors.red, width: 1.0))),
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return "Experience is required";
-                            } else if (int.tryParse(value) == null) {
-                              return "Experience must be a number";
-                            }
-                            return null;
-                          },
+                                  fontWeight: FontWeight.w500,
+                                )),
+                          ],
                         ),
-                        const SizedBox(height: 24.0),
-                        ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.white,
-                              shadowColor: const Color(0x261b0614),
-                              elevation: 10,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(50.0),
-                              ),
-                            ),
-                            onPressed: () async {
-                              FilePickerResult? result =
-                                  await FilePicker.platform.pickFiles(
-                                type: FileType.custom,
-                                allowedExtensions: ['jpg', 'pdf', 'doc'],
-                              );
-                              if (result != null) {
-                                setState(() {
-                                  _file = File(result.files.single.path!);
-                                });
-                              }
-                            },
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 24.0, vertical: 16.0),
-                              child: Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  Image.asset("assets/icons/upload.png"),
-                                  const SizedBox(width: 8.0),
-                                  const Text("Certification",
-                                      style: TextStyle(
-                                        color: AppColors.primaryColor,
-                                        fontSize: 16.0,
-                                        height: 1.5,
-                                        fontWeight: FontWeight.w500,
-                                      )),
-                                ],
-                              ),
+                      if (_file.path.isEmpty)
+                        const Text("No file choosen",
+                            style: TextStyle(
+                              color: AppColors.darkNeutrals500,
+                              fontSize: 14.0,
+                              height: 1.42,
+                              fontWeight: FontWeight.w500,
                             )),
-                        const SizedBox(height: 24.0),
-                        if (_file.path.isNotEmpty)
-                          Row(
-                            children: [
-                              Image.asset("assets/icons/upload.png"),
-                              const SizedBox(width: 8.0),
-                              Text(_file.path.split('/').last,
-                                  style: const TextStyle(
-                                    color: AppColors.primaryColor,
-                                    fontSize: 16.0,
-                                    height: 1.5,
-                                    fontWeight: FontWeight.w500,
-                                  )),
-                            ],
-                          ),
-                        if (_file.path.isEmpty)
-                          const Text("No file choosen",
-                              style: TextStyle(
-                                color: AppColors.darkNeutrals500,
-                                fontSize: 14.0,
-                                height: 1.42,
-                                fontWeight: FontWeight.w500,
-                              )),
-                      ])),
-              const Spacer(),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {}
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: AppColors.primaryColor,
-                    elevation: 16,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(32.0),
-                    ),
-                    shadowColor: const Color.fromRGBO(230, 126, 26, 0.15),
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 16.0, horizontal: 32.0),
+                    ])),
+            const Spacer(),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {}
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: AppColors.primaryColor,
+                  elevation: 16,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(32.0),
                   ),
-                  child: const Text("Next",
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        height: 1.5,
-                        fontWeight: FontWeight.bold,
-                        color: AppColors.light50,
-                      )),
+                  shadowColor: const Color.fromRGBO(230, 126, 26, 0.15),
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 16.0, horizontal: 32.0),
                 ),
+                child: const Text("Next",
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      height: 1.5,
+                      fontWeight: FontWeight.bold,
+                      color: AppColors.light50,
+                    )),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
